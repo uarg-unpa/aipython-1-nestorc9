1e2ecd092af1592eca2203bd9e24e0fc363a0497
nestorc9 <161514221+nestorc9@users.noreply.github.com>
2024-03-01 20:09:12 +0000

    trabajo en clase 01/3/24

---
+++ desarrollo guia 1/desarrollo del punto A - G.py
@@ -0,0 +1,6 @@
+print("Las maquinas me sorprenden con mucha frecuencia")
+print()
+print("hola")#mi propio mensaje
+print("\"23\"23")
+print("Una computadora puede ser llamada \"inteligente\" si logra engañar a una persona haciéndole creer que es un humano.")
+print("Néstor", "Cortez", "16 años")
---
+++ guia/principal.py
@@ -0,0 +1,2 @@
+nombre= "nestor" 
+print(nombre)
