eda2100843c74157566f779ad9065c332795f9ae
nestorc9 <161514221+nestorc9@users.noreply.github.com>
2024-03-08 00:23:49 +0000

    desarrollo guia 1 hasta el punto 5

---
+++ desarrollo guia 1/desarrollo del punto 3-4.py
@@ -0,0 +1,12 @@
+nombre=("nestor")
+apellido=("cortez")
+edad=16
+altura=12 
+número_de_vuelo=2321
+temperatura_del_ambiente=("20 grados")
+salario_mensual=("$100")
+fin_juego=("El juego ha terminado")
+print(f"{fin_juego}")
+numero_par=("Si tu número es divisible por dos sin resto es par")
+print(f"{numero_par}")
+#Las variables del ejercicio 3 se asocian a los literales str y int.
