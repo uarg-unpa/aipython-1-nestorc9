68e1c941d15b90f56b395763a584b0b46709adf5
nestorc9 <161514221+nestorc9@users.noreply.github.com>
2024-03-26 23:55:59 +0000

    desarrollo de la guia 3 hasta el punto 3

---
+++ desarrollo guia 3/desarrollo del punto 2.py
@@ -0,0 +1,4 @@
+n=[]
+for num in range(0,101):
+    n.append(num)
+print(n) 
---
+++ desarrollo guia 3/desarrollo del punto 3.py
@@ -0,0 +1,8 @@
+num=10
+while num > -1:
+    print(num)
+    num-=1
+#
+elem=[10,9,8,7,6,5,4,3,2,1,0]
+for n in elem:
+    print(n)
